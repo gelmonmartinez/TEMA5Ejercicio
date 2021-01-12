02509ce08564d3d80ba599e51ec53d16766bb8c4
gelmonmartinez <60948268+gelmonmartinez@users.noreply.github.com>
2021-01-12 18:01:27 -0500

    Add files via upload

---
+++ Ejemplo.java
@@ -0,0 +1,53 @@
+public class Programa_Ejemplo {
+
+   
+    /*este
+    es un comentario
+    multilinea segun la intruccion */
+    public static void main (String[] args) {
+    	
+    //Para la materia de
+    //Lenguajes y Automatas	
+    //Se usaran las siguientes valores.
+ 	int res1, res2, res3, res4;
+    int x = 10;
+    int y = 20;
+    int w = 15;
+    
+   
+    res1 = x + y;
+    res2 = y - w;
+    res3 = 2 * x;
+    res4 = y / x;
+    
+    suma = 0; 
+factorial = 1; 
+vidas = cont;
+
+suma = 2.7 + 3; 
+cont = cont + 1;
+resta = 2.7 - 3;
+
+edad >= 22; 
+suma != 0;
+altura <= 5;
+dia == 11;
+
+if(w == 8){
+int x = 4 - (2 + 9);
+}
+
+for (int i = i; i < numero; i++){
+int x = m + (cont + 4);
+
+}
+
+while(x =< 0){
+boolean w = cont + (cont - 2);
+
+}
+  
+    
+    			
+}
+}
